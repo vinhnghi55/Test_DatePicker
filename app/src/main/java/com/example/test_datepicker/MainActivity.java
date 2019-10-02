0a94dd5619a369328b11eed00eddc47ff7818074
vinhnghi55 <vinhnghi789@gmail.com>
2019-10-02 10:42:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_datepicker/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.test_datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datePicker = findViewById(R.id.date_picker);
+
+        init();
+    }
+    public void init(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                Toast.makeText(MainActivity.this, "hhhhhhhh", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
